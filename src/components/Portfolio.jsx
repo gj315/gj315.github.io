0e5d78d98a45e609509b161a01a29f9bbb3bc891
happy315 <singhjosan2317@gmail.com>
2022-03-28 15:50:51 +0530

    I make Portfolio project Filter Gallery

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Portfolio.css";
-const Portfolio = (props) =>{
+import Data from "./data";
+const Portfolio = () => {
+  const [items, setItems] = useState(Data);
+
+  const filterItems = (categoryItem) => {
+    const updatedItems = Data.filter((current) => {
+      return current.category === categoryItem;
+    });
+    setItems(updatedItems);
+  };
 
   return (
     <div className="portfolio" id="portfolio">
-      <div className="portfolio-section-header">
-        <div className="title">My Awesome Proects</div>
+      <h1 className="heading">My Awesome Projects</h1>
+      <div className="menu-tab-container">
+        <div className="menu-tab">
+          <button className="button" onClick={() => setItems(Data)}>
+            All
+          </button>
+          <button className="button" onClick={() => filterItems('Web')}>Web</button>
+          <button className="button" onClick={() => filterItems('Mobile')}>Mobile</button>
+          <button className="button" onClick={() => filterItems('BlockChain')}>BlockChain</button>
+          <button className="button" onClick={() => filterItems('Latest')}>Latest</button>
+        </div>
       </div>
-      <div className="portfolio-projects">
-        <div className="fillter-list">
-          <ul>
-            <li>All</li>
-            <li>Web</li>
-            <li>Mobile</li>
-            <li>BlockChain</li>
-          </ul>
-        </div>
-        <div className="projects-container">
-          
-        </div>
+      <div className="menu-items">
+        {/* my main item section    */}
+        {items.map((element) => {
+          return (
+            <div className="menu-item" key={element.id}>
+              <div className="image">
+                <img src={element.image} alt="" />
+              </div>
+              <div className="content">
+                <h2>{element.name}</h2>
+                <h3>{element.category}</h3>
+                <p>{element.description}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
