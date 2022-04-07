ac4e218a5534b370527002bc3558f4a476f8cefb
happy315 <singhjosan2317@gmail.com>
2022-04-07 15:30:18 +0530

    making contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,86 @@
-import React from "react";
-
+import React, { useState } from "react";
+import './Contact.css';
+import { BiMap } from 'react-icons/bi';
+import { AiFillPhone,AiOutlineMail } from 'react-icons/ai';
 const Contact = () => {
+
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const firstNameHandler = (e) => {
+     setFirstName(e.target.value)
+  }
+  const secondNameHandler = (e) => {
+    setLastName(e.target.value)
+  }
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value)
+  }
+  const messageChangeHandler = (e) => {
+    return (
+      setMessage(e.target.value)
+
+    )
+  }
+  
+   const onSubmitHandler = (e) => {
+     e.preventDefault();
+     setFirstName('');
+     setLastName('');
+     setEmail('');
+     setMessage('');
+    
+  }
+
   return (
-    <div className="contact" id="contact">
+    <div className="contact" id="contact" onSubmit={onSubmitHandler}>
       <h2 className="section-title">Contact us</h2>
-      <form action="">
-        <label htmlFor="Fname">First Name</label>
-        <input type="text" name="Fname" id="Fname" autoFocus />
-        <br />
-        <label htmlFor="Lname">Last Name</label>
-        <input type="text" name="Lname" id="Lname" />
-        <br />
-        <label htmlFor="email">Enter your Email</label>
-        <input type="email" name="email" id="email" />
-              <br />
-        <label htmlFor="message">Enter your message</label>
-        <input type="textarea" name="message" id="message" ></input>
-              
+
+      <div className="contact-content">
+        <div className="contact-info">
+          <div className="box">
+            <div className="icon"><BiMap /></div>
+            <div className="text">
+              <h3>Address</h3>
+              <p>Anupgarh,Rajasthan,India</p>
+            </div>
+          </div>
+          <div className="box">
+            <div className="icon"><AiOutlineMail /></div>
+            <div className="text">
+              <h3>Email</h3>
+              <p>gurpreet@guri.gmail.com</p>
+            </div>
+          </div>
+          <div className="box">
+            <div className="icon"><AiFillPhone /></div>
+            <div className="text">
+              <h3>Phone</h3>
+              <p>+919549626880</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form action="" className="contact-form">
+        <div >
+          <label htmlFor="Fname">First Name</label>
+          <input type="text" name="Fname" id="Fname" value={firstName}  onChange={firstNameHandler} />
+       </div>
+        <div>
+          <label htmlFor="Lname">Last Name</label>
+          <input type="text" name="Lname" id="Lname" value={lastName} onChange={secondNameHandler} required />
+        </div>
+        <div>
+          <label htmlFor="email">Enter your Email</label>
+          <input type="email" name="email" id="email" value={email} onChange={emailChangeHandler} required />
+        </div>
+        <div>
+          <label htmlFor="message">Enter your message</label> <br />
+          <textarea name="" id="" cols="30" rows="10" value={message} onChange={messageChangeHandler} placeholder="Type your message" required></textarea>
+        </div>
+        <button type="submit" value="send">Submit</button> 
       </form>
     </div>
   );
