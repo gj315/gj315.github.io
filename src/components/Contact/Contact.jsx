e8c6d732f117d9a7eeb9f50223f10add284c281a
happy315 <singhjosan2317@gmail.com>
2022-04-11 17:09:19 +0530

    make responsive contact me section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,13 +8,33 @@ const Contact = () => {
         <p>Let's Get in Touch And Make Miracle Together</p>
       </div>
       <div className="contact-form">
-        <form action="#">
-         
+        <form action="#" className="form">
+          <div className="box-container">
+            <div className="box">
+              <label htmlFor="fname">First Name</label>
+              <input type="text" name="fname" id="fname" />
+            </div>
+            <div className="box">
+              <label htmlFor="email">Your Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+          </div>
+          <div className="message">
+            <textarea
+              name="message"
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Enter your message"
+            ></textarea>
+          </div>
+          <div className="form-button">
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
     </div>
   );
-  
 };
 
 export default Contact;
