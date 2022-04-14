bc9ad29dcdd3fa3d8544c5d33144e91296ba5ff5
happy315 <singhjosan2317@gmail.com>
2022-04-14 15:54:26 +0530

    change images in portfolio data

--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -1,11 +1,12 @@
- const Data = [
-    {
+const Data = [
+   {
       id: "e1",
-      name: "Medical Research Web",
+      name: " Creative mindset web  App",
       category: "Web",
-      description: "An platform for medical research ",
-      image: "assets/medical-2.png",
+      description: "An digital agency website platform",
+      image: "assets/creative-mindset.png",
     },
+    
     {
       id: "e2",
       name: " Crypto Mobile App",
@@ -18,15 +19,15 @@
       id: "e3",
       name: "Crypto Wallet",
       category: "BlockChain",
-      description: "An  online Wallet",
-      image: "assets/blockchain-1.webp",
+      description: "An  online cryptocurrecy Wallet",
+      image: "assets/crypto-wallet.png",
     },
     {
       id: "e4",
-      name: "Make healthy",
-      category: "Web",
-      description: "An online medical service platform",
-      image: "assets/medical-1.png",
+      name: "Digital NFT",
+      category: "BlockChain",
+      description: "An online NFT selling or buying platform",
+      image: "assets/nft-marketplace.png",
     },
     {
       id: "e5",
@@ -37,16 +38,16 @@
     },
     {
       id: "e6",
-      name: "Latest",
-      category: "Web  ",
-      description: "An online medical service platform",
-      image: "assets/software-developer-g859bd614a_640.jpg",
+      name: "Modern crypto wallet",
+      category: "BlockChain",
+      description: "A modern crypto wallet",
+      image: "assets/crypto-wallet-2.png",
    },
    {
       id: "e7",
-      name: " Restuorent mobile App",
+      name: " Organic food  Mobile App",
       category: "Mobile",
-      description: "A  mobile  App platform",
+      description: "An online  platform for organic foods",
       image: "assets/resturent.webp",
    },
    {
@@ -58,32 +59,33 @@
    },
    {
       id: "e9",
-      name: " Maths game  App",
-      category: "Mobile",
-      description: "A math game mobile App platform",
-      image: "assets/game-app.webp",
+      name: "An call center",
+      category: "Latest",
+      description: "A call center website",
+      image: "assets/call-center.webp",
    },
    {
       id: "e10",
-      name: " Maths game  App",
-      category: "Mobile",
-      description: "A math game mobile App platform",
-      image: "assets/game-app.webp",
+      name: " An online payment solution  platform",
+      category: "Web",
+      description: "An online payment solution",
+      image: "assets/185db4140742745.Y3JvcCwyODAwLDIxOTAsMCww.jpg",
    },
    {
       id: "e11",
-      name: " Maths game  App",
-      category: "Crypto",
-      description: "A math game mobile App platform",
-      image: "assets/crpyto-2.webp",
+      name: " Fashion Theater",
+      category: "Web",
+      description: "A fashion website ",
+      image: "assets/fashion.png",
    },
    {
       id: "e12",
-      name: " Crypto token  App",
-      category: "Blockchain",
-      description: "A crypto wallet platform",
-      image: "assets/crypto-4.webp",
+      name: "SpeakLab",
+      category: "Latest",
+      description: "An Language course website ",
+      image: "assets/language-course.png",
     },
+   
  ];
 
 export default Data;
